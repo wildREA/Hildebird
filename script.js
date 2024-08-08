b29ef47358d2ba9efe62d9e64bd95bca1882c208
wildREA <wildrealinit@gmail.com>
2024-08-08 09:16:32 +0200

    moved files from pulled folder to local

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navLinks = document.querySelectorAll('#navigation a');
-    navLinks.forEach(link => {
-        link.addEventListener('click', function() {
-            navLinks.forEach(link => link.classList.remove('active'));
-            this.classList.add('active');
-        });
-    });
-});
