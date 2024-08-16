65532ac8bf533db5e3dd9b398e3a255de9e4d390
wildREA <wildrealinit@gmail.com>
2024-08-16 14:28:27 +0200

    updating files, finished dropdown and added divider for dropdown option pages

--- academic.php
+++ academic.php
@@ -9,7 +9,7 @@ require_once ("includes/header.php");
             <li id="list"><a href="portfolio.php">Portfolio</a></li>
             <li id="list"><a href="contact.php">Contact</a></li>
             <li id="dropdown"> 
-                <select name="info" id="selection">
+                <select id="selection" class="text-center" name="info">
                     <option id="active" href="academic.php" value="academic"><a href="">Akademisk</a></option>
                     <option value="development"><a href="">Udvikling</a></option>
                     <option value="design"><a href="">Design</a></option>
@@ -27,30 +27,12 @@ require_once ("includes/header.php");
                 <p id="date">Juni 2024</p>
             </article>
         </section>
-        <br>
+        <hr id="divider" class="hr-blurry" />
         <section id="information">
             <h3 id="certificate">Certificeringer</h3>
             <article id="officials">
                 <p>SCD - Object Oriented Programming Certified Developer | 2022</p>
                 <p id="python">SCD - Python Certified Developer | 2020</p>
-            </article>
-        </section>
-        <br>
-        <section id="information">
-            <h3 id="qualifications">Kvalifikationer</h3>
-            <article id="paragraphs">
-                <h4 id="development">Udvikling</h4>
-                <div id="descriptions">
-                    <p id="misfit">Jeg arbejder mest med udvikling af themes og plugins til WordPress. De WordPress hjemmesider, jeg laver til mine kunder, er fuldt skræddersyede. Du kan se mine open-source bidrag på GitHub. PHP er mit primære serverside-sprog, som jeg ofte anvender sammen med  MySQL. Jeg har også arbejdet i Node med MongoDB. Laravel har jeg hovedsagligt benyttet til enkeltstående API-projekter.</p>
-                    <p>Jeg bygger apps med React Native til Android og iOS. Du kan finde mine apps XX og YY på app store. Jeg bruger HTML og CSS dagligt. Jeg arbejder seriøst med tilgængelighed. Jeg holdre mig opdateret om det nyeste, men sørger for at min kode er bagud-kompatibel.</p>
-                    <p>JavaScript bruger jeg også hver dag, ofte med React. Jeg har udviklet Node.js-baseret workflow automatisering via Webpack, som jeg bruger til mine web-projekter. Jeg bruger Git til alle mine programmeringsopgaver. Tag et kig på min GitHub , hvor du kan finde forskellige projekter og mine bidrag til forskellige open source projekter.</p>
-                    <p id="spacing">Jeg står selv for mine servere, så jeg har godt styr på kommandoprompten. Jeg håndterer ofte DNS. Jeg koder helst i Microsoft  VS Code eller i Apache NetBeans.</p>
-                </div>
-                <h4 id="development">Design</h4>
-                <div id="descriptions">
-                    <p id="misfit">Min designproces begynder med wireframing og fortsætter med råskitser, som jeg udvikler til en prototype.  Efter flere iterationer og i samarbejde med kunden, udvikler jeg det endelige design.</p> <br>
-                    <p id="spacing_last">Jeg har professionel erfaring med Adobe Creative Cloud, især XD, Illustrator og Photoshop.</p>
-                </div>
             </article>
         </section>
     </main>
