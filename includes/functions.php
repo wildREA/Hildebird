795d22848544ada691fc0edc0e3cfe3664c9b108
wildREA <wildrealinit@gmail.com>
2024-08-26 10:21:55 +0200

    updating files

--- includes/functions.php
+++ includes/functions.php
@@ -1,12 +1,4 @@
 <?php
-
-function problem($error) {
-    echo "It looks like there is a problem with your form data: <br><br>";
-    echo $error . "<br><br>";
-    echo "Fix those to proceed.<br><br>";
-    die();
-}
-
 function clean_string($string) {
     $bad = array("content-type", "bcc:", "to:", "cc:", "href");
     return str_replace($bad, "", $string);
